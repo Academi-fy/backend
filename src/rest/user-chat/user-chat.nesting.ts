5bd24016759e9d304ddf9543a9c3108a1a0c9012
Daanieeel <d4niel.dd060306@gmail.com>
2024-03-08 13:24:01 +0100

    Update user-chat.nesting.ts

--- src/rest/user-chat/user-chat.nesting.ts
+++ src/rest/user-chat/user-chat.nesting.ts
@@ -1,6 +1,20 @@
+import { userRawNesting } from '@/rest/user';
+import { chatRawNesting } from '@/rest/chat';
+
 export const userChatRawNesting = {
   chat: true,
   user: true,
 } as const;
 
-export const userChatNesting = {} as const;
+export const userChatNesting = {
+  chat: {
+    include: {
+      ...chatRawNesting,
+    },
+  },
+  user: {
+    include: {
+      ...userRawNesting,
+    },
+  },
+} as const;
