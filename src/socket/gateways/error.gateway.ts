a21516268fb1acc36489b3333a0ae9bbd3412d12
Daanieeel <d4niel.dd060306@gmail.com>
2024-06-07 13:39:42 +0200

    update

--- src/socket/gateways/error.gateway.ts
+++ src/socket/gateways/error.gateway.ts
@@ -7,14 +7,21 @@ import { SOCKET_PORT } from '@/constants';
 import { Gateway } from '@/socket/entities/gateway.entity';
 import { GatewayMessage } from '@/socket/entities/gateway-message.entity';
 import { GatewayResponse } from '@/socket/entities/gateway-response.entity';
+import { Logger, Req } from '@nestjs/common';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway(SOCKET_PORT)
 export class ErrorGateway extends Gateway {
+  public errorLogger: Logger = new Logger('ERROR_GATEWAY');
+
   @SubscribeMessage('ERROR')
   async handleError(
     @MessageBody() body: GatewayMessage<Error>,
-  ): Promise<GatewayResponse> {
-    console.error(body);
+    @Req() request: Socket,
+  ): Promise<GatewayResponse | void> {
+    this.errorLogger.error(
+      `Error received from client '${request.handshake.query.userId}': \n${body}`,
+    );
     return new GatewayResponse(true, '1', body);
   }
 }
