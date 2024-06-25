46a6bfb7a474d89daf06365be1c98e91c4f75a49
Daanieeel <d4niel.dd060306@gmail.com>
2024-06-25 16:36:00 +0200

    chat caching deactivated

--- src/rest/chat/services/chat.service.ts
+++ src/rest/chat/services/chat.service.ts
@@ -4,10 +4,14 @@ import { Service } from '@/service';
 import { ChatCacheService } from '@/rest/chat/services/chat-cache.service';
 import { Chat } from '@/@generated-types';
 import { CreateChatDto, EditChatDto } from '@/rest/chat';
+import { ChatDatabaseService } from '@/rest/chat/services/chat-database.service';
 
 @Injectable()
 export class ChatService extends Service {
-  constructor(private chatCacheService: ChatCacheService) {
+  constructor(
+    private chatCacheService: ChatCacheService,
+    private chatDatabaseService: ChatDatabaseService,
+  ) {
     super();
   }
 
@@ -16,7 +20,7 @@ export class ChatService extends Service {
    * the database and store them in cache.
    */
   async getAllChats(): Promise<Chat[]> {
-    return this.chatCacheService.getAllChats();
+    return this.chatDatabaseService.getAllChats(); //TODO back to caching
   }
 
   /**
@@ -24,27 +28,27 @@ export class ChatService extends Service {
    * it from the database and store it in cache.
    */
   async getChatById(id: string): Promise<Chat> {
-    return this.chatCacheService.getChatById(id);
+    return this.chatDatabaseService.getChatById(id); //TODO back to caching
   }
 
   /**
    * @description Create a new chat and store it in cache.
    */
   async createChat(createChatDto: CreateChatDto): Promise<Chat> {
-    return this.chatCacheService.createChat(createChatDto);
+    return this.chatDatabaseService.createChat(createChatDto); //TODO back to caching
   }
 
   /**
    * @description Edit an existing chat and update it in cache.
    */
   async editChat(chatId: string, editChatDto: EditChatDto): Promise<Chat> {
-    return this.chatCacheService.editChat(chatId, editChatDto);
+    return this.chatDatabaseService.editChat(chatId, editChatDto); //TODO back to caching
   }
 
   /**
    * @description Delete a chat from cache.
    */
   async deleteChat(chatId: string): Promise<Chat> {
-    return this.chatCacheService.deleteChat(chatId);
+    return this.chatDatabaseService.deleteChat(chatId); //TODO back to caching
   }
 }
